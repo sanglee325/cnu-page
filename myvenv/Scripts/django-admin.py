c2c3fd03fcb655a0b5872cf431e9c16c910b3aed
sanglee <pksll325@gmail.com>
2019-02-12 15:32:46 +0900

    environment setting

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\loyola\documents\vscode\django-lab-manange\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
