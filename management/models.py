93b6457e4aeba912f21e5ab1b2fb603fdbd90dd0
sanglee <pksll325@gmail.com>
2019-02-15 17:38:47 +0900

    Model: members added

--- management/models.py
+++ management/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django import forms
+from members import models as members_models
 
 # Create your models here.
-class Manager(models.Model):
-    student = models.CharField(max_length=20)
-
+"""
+class SomeModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(SomeModelForm, self).__init__(*args, **kwargs)
+        self.fields['some_field'].widget = forms.CheckboxSelectMultiple()
+"""
+    
 class Period(models.Model):
-    student1 = Manager()
-    student2 = Manager()
+    student1 = members_models.Person()
+    student2 = members_models.Person()
+
+
 
     
