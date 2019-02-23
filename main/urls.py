29b46959032c40d00831cac09098ccdf97955462
sanglee <pksll325@gmail.com>
2019-02-23 23:23:27 +0900

    urls changed

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^index/$', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('manage', views.manage, name='manage'),
+    path('contact', views.contact, name='contact'),
     #url(r'^logout/$', views.logout, name='logout'),
     #url(r'^signup/$', views.signup, name='signup'), #only signup page ready
 ]
--- main/views.py
+++ main/views.py
@@ -6,3 +6,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html', {'index': index})
+
+def about(request):
+    return render(request, 'main/about.html', {'about': about})
+
+def manage(request):
+    return render(request, 'main/building.html', {'manage': manage}) 
+
+def contact(request):
+    return render(request, 'main/building.html', {'contact': contact})
